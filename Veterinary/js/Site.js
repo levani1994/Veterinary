4dc4f7a1cbc696d10af42a74275afe6990a67c66
Nikakakhiani <Nikakakhiani@users.noreply.github.com>
2019-07-25 19:38:37 +0400

    Ajax Login Method (incomplite)
    Ajax Login Method (incomplite)

--- Veterinary/js/Site.js
+++ Veterinary/js/Site.js
@@ -19,14 +19,30 @@
             success: function () {
                 $('#CloseBtn').trigger('click');
                 $('#ProductName').val('');
-
             }
-
-
-
-
         });
     });
 
+    $('#LoginBtn').click(function () {
+        var data = {};
+
+        data.Email = $('#LoginEmail').val();
+        data.Password = $('#LoginPassword').val();
+        $.ajax({
+            url: '/Account/Login',
+            method: 'POST',
+            data: {
+                model: data,
+                returnUrl: 'Home/Index'
+            },
+            success: function () {
+                alert('success')
+            },
+            error: function () {
+                alert('Login Failed')
+            },
+        });
+        
+    });
 })
 
